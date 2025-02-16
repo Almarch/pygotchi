ae31ddea886fda424b23bea5adf426446462bc0a
Almarch <alexandre.marchal@hotmail.com>
2025-02-16 15:18:54 +0100

    flash fixed - dump to test

--- pygotchi/Tama.py
+++ pygotchi/Tama.py
@@ -29,26 +29,31 @@ class Tama(CppTama):
         obj = self.GetROM()
         obj = [f"{num:02X}" for num in obj]
         obj = "".join(obj)
-        bin = []
+        bin_data = []
         j = 0
         for i in range(len(obj) + len(obj) // 3):
             if (i + 3) % 4 == 0:
-                bin.append("0")
+                bin_data.append("0")
             else:
-                bin.append(obj[j])
+                bin_data.append(obj[j])
                 j += 1
-        bin = "".join(bin)
-        bin = bytes.fromhex(bin)
-        return(bin)
+        bin_data = "".join(bin_data)
+        bin_data = bytes.fromhex(bin_data)
+        return bin_data
 
     def flash(self, bin):
         obj = "".join(f"{byte:02X}" for byte in bin)
-        obj = "".join(obj[i] for i in range(len(obj)) if (i + 3) % 4)
-        obj = [obj[2 * i: 2 * i + 2] for i in range(len(obj) // 2)]
-        obj = [int(hex_str, 16) for hex_str in obj]
+        filtered_obj = []
+        for i in range(len(obj)):
+            if(i % 4 == 0):
+                continue
+            else:
+                filtered_obj.append(obj[i])
+        filtered_obj
+        obj = "".join(filtered_obj)
+        obj = [int(obj[2 * i: 2 * i + 2], 16) for i in range(len(obj) // 2)]
         self.SetROM(obj)
-
-    
+        
 
 
 
